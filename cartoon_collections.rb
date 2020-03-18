4c5824af7839401b988f26bc9387e6a350030363
maxwellbenton <maxwellbenton@gmail.com>
2020-03-18 17:06:03 -0400

    set starter code

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,11 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def greet_characters(array)
+  # Use `each` to enumerate over the provided array
+  #
+  # Print a custom greeting for each element
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
-end
-
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def list_dwarves(array)
+  # Use `each_with_index` to enumerate over the provided array
+  #
+  # Print a numbered list of each element
 end
